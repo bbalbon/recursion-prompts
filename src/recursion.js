4f1d54e47fd0ce41c7281d64e76066cad60ca2fe
Bryce Balbon <bbalbon@gmail.com>
2019-11-29 15:00:56 +1300

    Complete test prompts

--- src/recursion.js
+++ src/recursion.js
@@ -147,7 +147,10 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
-    if (x > y) {
+    if (y === 0) {
+        return NaN
+    }
+    if (x >= y) {
         return modulo((x-y), y);
     } else {
         return x;
@@ -157,11 +160,33 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+    if (y < x) {
+        return multiply(y, x);
+    }
+    if (x < 0 && y < 0) {
+        return multiply(-y, -x);
+    }
+    if (y === 0) {
+        return 0;
+    } else {
+        return x + multiply(x, (y-1));
+    }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+    if (y === 0) {
+        return NaN;
+    }
+    if (x < 0 && y < 0) {
+        return divide(-x, -y);
+    }
+    if (x < y) {
+        return 0;
+    } else {
+        return 1 + divide((x-y), y);
+    }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
@@ -170,6 +195,17 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+    if (x < 0 || y < 0) {
+        return null;
+    }
+    if (x === 0) {
+        return y;
+    } 
+    else if (y === 0) {
+        return x;
+    } else {
+        return gcd(y, (x % y));
+    }
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
@@ -177,6 +213,14 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+    if (str1.length === 0 && str2.length === 0) {
+        return true;
+    }
+    if (str1[0] !== str2[0]) {
+        return false;
+    } else {
+        return compareStr(str1.slice(1), str2.slice(1));
+    }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
