ed8364f4fa595c37ac6c16f1540cfd72f4263036
Bryce Balbon <bbalbon@gmail.com>
2019-12-01 13:25:49 +1300

    Update recursion prompt solutions

--- src/recursion.js
+++ src/recursion.js
@@ -319,7 +319,16 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
-    
+    let count = 0;
+    for (let prop in obj) {
+        if (prop === key) {
+            count++;
+        } 
+        if (typeof obj[prop] === 'object') {
+            count += countKeysInObj(obj[prop], key);
+        }
+    }
+    return count;
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -327,11 +336,33 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+    let count = 0;
+    for (let key in obj) {
+        const val = obj[key];
+        if (val === value) {
+            count++;
+        }
+        if (typeof val === 'object') {
+            count += countValuesInObj(val, value);
+        }
+    }
+    return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+    for (let key in obj) {
+        let value = obj[key];
+        if (key === oldKey) {
+            obj[newKey] = value;
+            delete obj[key];
+        }
+        if (typeof value === 'object') {
+            value = replaceKeysInObj(value, oldKey, newKey);
+        }
+    }
+    return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
@@ -340,6 +371,16 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+    if (n <= 0) {
+        return null;
+    }
+    if (n === 1) {
+        return [0, 1];
+    } else {
+        var s = fibonacci(n - 1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -348,17 +389,40 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+    if (n === 0) {
+        return 0;
+    }
+    else if (n < 0) {
+        return null;
+    }
+    else if (n === 1 || n === 2) {
+        return 1;
+    } else {
+        return nthFibo(n-1) + nthFibo(n-2);
+    }
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 var capitalizeWords = function(array) {
+    if (array.length === 0) {
+        return [];
+    } else {
+        result = [array[0].toUpperCase()];
+        return result.concat(capitalizeWords(array.slice(1)));
+    }
 };
 
 // 28. Given an array of strings, capitalize the first letter of each index.
 // capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
 var capitalizeFirst = function(array) {
+    if (array.length === 0) {
+        return [];
+    } else {
+        const result = [array[0][0].toUpperCase() + array[0].slice(1)];
+        return result.concat(capitalizeFirst(array.slice(1)));
+    }
 };
 
 // 29. Return the sum of all even numbers in an object containing nested objects.
@@ -371,16 +435,43 @@ var capitalizeFirst = function(array) {
 // };
 // nestedEvenSum(obj1); // 10
 var nestedEvenSum = function(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        let value = obj[key];
+        if (value % 2 === 0) {
+            sum += value;
+        }
+        if (typeof value === 'object') {
+            sum += nestedEvenSum(value);
+        }
+    }
+    return sum;
 };
 
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+    let res = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!(Array.isArray(array[i]))) {
+            res.push(array[i]);
+        } else {
+            res = res.concat(flatten(array[i]));
+        }
+    }
+    return res;
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+    let letter = str[0];
+    if (str.length === 0) {
+        return obj;
+    } else {
+        obj[letter] ? obj[letter] ++ : obj[letter] = 1;
+        return letterTally(str.slice(1), obj);
+    }
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
@@ -428,6 +519,23 @@ var tagCount = function(tag, node) {
 // binarySearch(array, 5) // 5
 // https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
 var binarySearch = function(array, target, min, max) {
+    min = (min || min === 0) ? min : 0;
+    max = (max || max === 0) ? max : array.length - 1;
+    let midpoint = Math.floor((min + max)/2);
+
+    if (max < min) {
+        return null;
+    }
+    else if (array[midpoint] === target) {
+        return midpoint;
+    }
+    else if (target > array[midpoint]) {
+        min = midpoint + 1;
+    }
+    else if (target < array[midpoint]) {
+        max = midpoint - 1;
+    }
+    return binarySearch(array, target, min, max);
 };
 
 // 39. Write a merge sort function.
